eefbf9a7dba71c2b7e0826446a2a1921a4e039a1
Jaskaranjit Singh <singhjaskar1@gmail.com>
2015-11-06 12:35:35 -0500

    demo map is up

--- app/controllers/resturants_controller.rb
+++ app/controllers/resturants_controller.rb
@@ -6,12 +6,15 @@ class ResturantsController < ApplicationController
     config.token = ENV["YELP_TOKEN"]
     config.token_secret = ENV["YELP_TOKEN_SECRET"]
   end
-def info
- responses = Yelp.client.search('East Village, NY', { term: 'resturant', sort: 2})
- render json: responses
-end
-
+  def info
+   responses = Yelp.client.search('East Village, NY', { term: 'resturant', sort: 2})
+   render json: responses
+  end
 
   def index
+  end
+
+  def map
+
   end
 end
