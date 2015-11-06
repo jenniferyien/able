897ce8ff6d2617bed8f381a4c8f33941b9da3abc
Jennifer Yien <jenniferllwu@gmail.com>
2015-11-06 17:13:19 -0500

    change

--- app/assets/javascripts/yelping.js
+++ app/assets/javascripts/yelping.js
@@ -39,25 +39,25 @@ $(function(){
       google.maps.event.addDomListener(window, 'load', initMap());
        $('#results').empty()
 
-      //  data.businesses.forEach(function(business){
-      //    var link = "/resturants/"+business.phone
-      //    var info = $('<div class="panel panel-default"/>')
-      //    var heading = $('<div class="panel-heading"><a href='+link +'>'+business.name+'</a></div>')
-      //    var body = $('<div class="panel-body"/>')
-      //    var image = $('<div class="col-md-3"><img src='+business.image_url+'></div>')
-      //    var base = $('<div class="col-md-8"><p>Yelp Rating:'+ business.rating+' </p><p>Number of Yelp Reviews:' + business.review_count +'</p><p>Food Type:'+ business.categories +' </p><p>Address: '+business.location.display_address +'</p><p>Phone Number: '+business.phone +'</p></div>')
-      //    image.appendTo(body)
-      //    base.appendTo(body)
-      //    heading.appendTo(info)
-      //    body.appendTo(info)
-      //
-      //    info.appendTo('#results')
-      //  })
-      //
-      // },
-      // error: function(xhr, data, error){
-      //   console.log("error is "+ error)
-      // }
+       data.businesses.forEach(function(business){
+         var link = "/resturants/"+business.phone
+         var info = $('<div class="panel panel-default"/>')
+         var heading = $('<div class="panel-heading"><a href='+link +'>'+business.name+'</a></div>')
+         var body = $('<div class="panel-body"/>')
+         var image = $('<div class="col-md-3"><img src='+business.image_url+'></div>')
+         var base = $('<div class="col-md-8"><p>Yelp Rating:'+ business.rating+' </p><p>Number of Yelp Reviews:' + business.review_count +'</p><p>Food Type:'+ business.categories +' </p><p>Address: '+business.location.display_address +'</p><p>Phone Number: '+business.phone +'</p></div>')
+         image.appendTo(body)
+         base.appendTo(body)
+         heading.appendTo(info)
+         body.appendTo(info)
+
+         info.appendTo('#results')
+       })
+
+      },
+      error: function(xhr, data, error){
+        console.log("error is "+ error)
+      }
     });
     })
     $("#addReview").click(function(){
