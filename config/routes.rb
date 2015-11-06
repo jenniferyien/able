35372de501d9a477802de559a182400e617adfcd
Jaskaranjit Singh <singhjaskar1@gmail.com>
2015-11-06 16:50:50 -0500

    trying to deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-   
-
+  # See how all your routes lay out with "rake routes". 
   # omniauth callback
   get 'auth/:provider/callback', to: 'sessions#create'
   # signout user
